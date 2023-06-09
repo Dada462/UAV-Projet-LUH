3d54ba5073275a002182453d1df53aac4bbdc9ab
Dada462 <danut462@gmail.com>
2023-06-09 14:04:24 +0200

    update

--- controller_tools/src/controller_tools/ActionServer.py
+++ controller_tools/src/controller_tools/ActionServer.py
@@ -1,8 +1,10 @@
+#!/usr/bin/env python
 import rospy
 import actionlib
 from uavr_nav_msgs.msg import FollowPathAction, FollowPathFeedback, FollowPathResult
 from std_msgs.msg import String, Float32MultiArray
 from numpy import array, inf
+
 
 class ActionServer():
     def __init__(self, pfc=None):
@@ -14,7 +16,7 @@ class ActionServer():
             '/user_input', String, queue_size=10)
         self.user_input = rospy.Subscriber(
             '/user_input', String, self.userInputCallback)
-        self.path_pub=rospy.Publisher(
+        self.path_pub = rospy.Publisher(
             '/path', Float32MultiArray, queue_size=1)
         self.userInput = ''
         self.path = None
@@ -34,20 +36,20 @@ class ActionServer():
         points = []
         speeds = []
         headings = []
-        for i,pose in enumerate(poses):
+        for i, pose in enumerate(poses):
             points.append([pose.position.x, pose.position.y, pose.position.z])
-            speed=velocities[i].linear.x
-            heading=velocities[i].angular.z
+            speed = velocities[i].linear.x
+            heading = velocities[i].angular.z
             speeds.append(speed)
             headings.append(heading)
         points = array(points).T
-        path_points=Float32MultiArray()
-        path_points.data=points.flatten()
+        path_points = Float32MultiArray()
+        path_points.data = points.flatten()
         self.path_pub.publish(path_points)
         while self.userInput != 'FOLLOWPATH' and not rospy.is_shutdown():
             self.user_input_pub.publish(String('FOLLOWPATH'))
             r.sleep()
-        self.pfc.init_path(points,speeds,headings)
+        self.pfc.init_path(points, speeds, headings)
         pathInterrupted = False
         while self.distance_to_goal > 0.1 and not rospy.is_shutdown():
             if self.server.is_preempt_requested() or self.userInput != 'FOLLOWPATH':
